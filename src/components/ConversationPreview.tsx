0097877088b7c9a044e511e51f31b79e5c7ea931
Jonathan May <jmay4826@gmail.com>
2018-09-20 23:42:31 -0600

    begin converting dashboard and conversationpreview to MUI

--- src/components/ConversationPreview.tsx
+++ src/components/ConversationPreview.tsx
@@ -1,16 +1,23 @@
 import * as React from "react";
 import { Link } from "react-router-dom";
+import { ListItem, Avatar, ListItemText } from "@material-ui/core";
+import { default as EmailIcon } from "@material-ui/icons/Email";
 
 const ConversationPreview = ({ sent_at, users, content, id }: any) => {
   return (
-    <Link className="conversation-preview" to={`/conversations/${id}`}>
-      {users.map((user: string) => (
-        <span className="conversation-users" key={user}>
-          {user}
-        </span>
-      ))}
-      <p>{content}</p>
-      <p>Last Message: {new Date(sent_at).toLocaleString()}</p>
+    <Link to={`/conversations/${id}`}>
+      <ListItem button={true}>
+        <Avatar>
+          <EmailIcon />
+        </Avatar>
+        <ListItemText
+          primary={users.reduce(
+            (acc: string, user: string, i: number, arr: string[]) =>
+              i < arr.length - 1 ? `${acc} ${user},` : `${acc} ${user}`
+          )}
+          secondary={new Date(sent_at).toLocaleString()}
+        />
+      </ListItem>
     </Link>
   );
 };
--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -2,6 +2,7 @@ import Axios from "axios";
 import * as React from "react";
 import { RouteComponentProps } from "react-router";
 import { ConversationPreview } from "./ConversationPreview";
+import { List } from "@material-ui/core";
 
 interface IState {
   [key: string]: any;
@@ -32,10 +33,13 @@ class Dashboard extends React.Component<IProps, IState> {
   public render() {
     return (
       <div>
+        <nav>Logout</nav>
         <h1>Conversations</h1>
-        {this.state.conversations.map((conversation: any) => (
-          <ConversationPreview key={conversation.id} {...conversation} />
-        ))}
+        <List>
+          {this.state.conversations.map((conversation: any) => (
+            <ConversationPreview key={conversation.id} {...conversation} />
+          ))}
+        </List>
         <p>{JSON.stringify(this.state.error)}</p>
       </div>
     );
