79cf18e5c0a01ec57b7942799b9089ec4560bc3c
Jonathan May <jmay4826@gmail.com>
2018-09-25 15:12:39 -0600

    Add student filter for InstructorDashboard

--- src/components/Students.tsx
+++ src/components/Students.tsx
@@ -3,12 +3,14 @@ import Axios from "axios";
 import * as React from "react";
 
 import { StudentConversations } from "./StudentConversations";
+import { TextField } from "@material-ui/core";
 
 class Students extends React.Component<any, any> {
   constructor(props: any) {
     super(props);
     this.state = {
       error: "",
+      filter: "",
       students: []
     };
   }
@@ -17,19 +19,24 @@ class Students extends React.Component<any, any> {
       headers: { Authorization: `Bearer ${localStorage.getItem("token")}` }
     })
       .then(({ data: { students } }) => {
-        console.log(students);
         this.setState({ students });
       })
       .catch(error => this.setState({ error }));
   }
+
   public render() {
     return (
       <div>
         <h1>My Students</h1>
+        <TextField onChange={e => this.setState({ filter: e.target.value })} />
         <List>
-          {this.state.students.map((student: any) => (
-            <StudentConversations {...student} />
-          ))}
+          {this.state.students
+            .filter((student: any) =>
+              student.username.includes(this.state.filter)
+            )
+            .map((student: any) => (
+              <StudentConversations key={student.user_id} {...student} />
+            ))}
         </List>
       </div>
     );
