85b5a1ef55fb2163ff94ef0e9babe170c804fe78
Jonathan May <jmay4826@gmail.com>
2018-09-25 15:12:20 -0600

    Reuse ConversationPreview for StudentConversations

--- src/components/StudentConversations.tsx
+++ src/components/StudentConversations.tsx
@@ -1,46 +1,50 @@
-import * as React from "react";
-import { List, ListItem, ListItemText, Collapse } from "@material-ui/core";
+import Collapse from "@material-ui/core/Collapse";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
 import ExpandLess from "@material-ui/icons/ExpandLess";
 import ExpandMore from "@material-ui/icons/ExpandMore";
-import { Redirect } from "react-router";
+import * as React from "react";
+import { ConversationPreview } from "./ConversationPreview";
+import { IConversation } from "./Conversations";
+import { Typography } from "@material-ui/core";
 
-class StudentConversations extends React.Component<any, any> {
-  constructor(props: any) {
+interface IState {
+  open: boolean;
+}
+
+interface IProps {
+  conversations: IConversation[];
+  username: string;
+}
+
+class StudentConversations extends React.Component<IProps, IState> {
+  constructor(props: IProps) {
     super(props);
     this.state = {
-      open: false,
-      conversation: 0
+      open: false
     };
   }
 
-  render() {
-    console.log(this.props);
-    return this.state.conversation ? (
-      <Redirect to={`conversations/${this.state.conversation}`} push={true} />
-    ) : (
+  public toggleOpen = () => {
+    this.setState(prev => ({ open: !prev.open }));
+  };
+
+  public render() {
+    return (
       <React.Fragment>
-        <ListItem
-          onClick={() => this.setState((prev: any) => ({ open: !prev.open }))}
-        >
+        <ListItem onClick={this.toggleOpen}>
           <ListItemText primary={this.props.username} />
           {this.state.open ? <ExpandLess /> : <ExpandMore />}
         </ListItem>
         <Collapse in={this.state.open}>
           <List>
-            {this.props.conversations.map((conversation: any) => (
-              <ListItem
-                onClick={() =>
-                  this.setState({ conversation: conversation.conversation_id })
-                }
-              >
-                <ListItemText
-                  inset={true}
-                  primary={conversation.participants.reduce(
-                    (acc: string, part: string) => `${acc}, ${part}`
-                  )}
-                />
-              </ListItem>
+            {this.props.conversations.map(conversation => (
+              <ConversationPreview {...conversation} key={conversation.id} />
             ))}
+            {!this.props.conversations.length && (
+              <Typography color="error">No conversations found</Typography>
+            )}
           </List>
         </Collapse>
       </React.Fragment>
