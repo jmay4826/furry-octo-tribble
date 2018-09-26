5129f743b55d01205d0b938caee21a5f7484cad5
Jonathan May <jmay4826@gmail.com>
2018-09-26 11:12:19 -0600

    Add sidebar to PrivateRoutes

--- src/PrivateRoute.tsx
+++ src/PrivateRoute.tsx
@@ -1,10 +1,33 @@
+import {
+  faComments,
+  faSignOutAlt,
+  faUser
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import * as React from "react";
 import { Redirect, Route } from "react-router";
 
 const PrivateRoute = (props: any) => {
   const render = (routerProps: any) =>
     props.authenticated ? (
-      <props.component {...routerProps} />
+      <div className="container">
+        {/* TODO: Move to separate component and add labels */}
+        <div className="sidebar">
+          <FontAwesomeIcon
+            icon={faComments}
+            size="2x"
+            className="sidebar-icon"
+          />
+          <FontAwesomeIcon icon={faUser} size="2x" className="sidebar-icon" />
+          <FontAwesomeIcon
+            icon={faSignOutAlt}
+            size="2x"
+            className="sidebar-icon"
+          />
+        </div>
+
+        <props.component {...routerProps} />
+      </div>
     ) : props.loading ? null : (
       <Redirect to="/" />
     );
