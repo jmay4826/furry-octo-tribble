ce0f6f14638b66a5437574a052bf7b5ddb787fab
Jonathan May <jmay4826@gmail.com>
2018-09-26 21:30:16 -0600

    Remove MUI

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,8 +1,7 @@
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
-import Typography from "@material-ui/core/Typography";
+// import Card from "@material-ui/core/Card";
+// import CardContent from "@material-ui/core/CardContent";
+// import Typography from "@material-ui/core/Typography";
 import * as React from "react";
-// import { IMessage } from "..";
 
 interface IMessageWithUser extends IMessage {
   current_user: string;
@@ -16,23 +15,21 @@ const Message = ({
 }: IMessageWithUser) => {
   return (
     <div
-      style={{
-        alignItems: from_username === current_user ? "flex-end" : "flex-start",
-        display: "flex",
-        flexDirection: "column",
-        margin: "10px",
-        whiteSpace: "pre-line"
-      }}
+      className={`message-container ${
+        from_username === current_user ? "right" : "left"
+      }`}
     >
-      <Card>
-        <CardContent>
-          <Typography variant="headline">{from_username}</Typography>
-          <Typography variant="subheading">
-            {new Date(sent_at).toLocaleString()}
-          </Typography>
-          <p>{content}</p>
-        </CardContent>
-      </Card>
+      <div
+        className={`message-content ${
+          from_username === current_user ? "right" : "left"
+        }`}
+      >
+        {content}
+      </div>
+      <div className="message-info-container">
+        <span className="bold">{from_username}</span> &bull;{" "}
+        {new Date(sent_at).toLocaleString()}
+      </div>
     </div>
   );
 };
