74b74361d61b9aa9288a85380f38b8f8f4953035
Jonathan May <jmay4826@gmail.com>
2018-10-06 19:57:05 -0600

    Only use ... for longer messages

--- src/components/ConversationPreview.tsx
+++ src/components/ConversationPreview.tsx
@@ -25,7 +25,11 @@ const ConversationPreview = ({
           )}
         </p>
         <p className="conversation-preview-content">
-          {content ? content.substring(0, 50) + "..." : "No messages yet"}
+          {content
+            ? content.length < 50
+              ? content
+              : content.substring(0, 50) + "..."
+            : "No messages yet"}
         </p>
         <p className="conversation-preview-content">
           {sent_at && <TimeAgo date={sent_at} />}
