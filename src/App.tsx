3625e368e1a3c3718771bfdae979438fab4d0097
Jonathan May <jmay4826@gmail.com>
2018-10-11 20:23:36 -0600

    Add sample privacy policies

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import { Navbar } from "./components/Navbar";
 import { PrivateRoute } from "./components/PrivateRoute";
 import { Sections } from "./components/Sections";
 import { SignUp } from "./components/SignUp";
+import { Privacy } from "./components/Privacy";
 
 export const UserContext = React.createContext({} as IDecodedUser);
 
@@ -90,6 +91,7 @@ class App extends React.Component<{}, IState> {
             <Route path="/signup" component={SignUp} />
             <Route path="/login" render={this.loginComponent} />
             <Route path="/logout" render={this.logoutComponent} />
+            <Route path="/privacy" component={Privacy} />
             {this.state.user && (
               <UserContext.Provider value={this.state.user}>
                 <div className="container">
