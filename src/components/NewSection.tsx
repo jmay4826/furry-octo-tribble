cfe1217bf357c4a3b4e31d38802f408afbb33c12
Jonathan May <jmay4826@gmail.com>
2018-10-14 12:05:04 -0600

    Fix linting errors

--- src/components/NewSection.tsx
+++ src/components/NewSection.tsx
@@ -1,10 +1,10 @@
 import { faFileUpload } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Axios from "axios";
+import * as _ from "lodash";
+import * as Papa from "papaparse";
 import * as React from "react";
 import { RouteComponentProps } from "react-router";
-import * as Papa from "papaparse";
-import * as _ from "lodash";
 
 interface IParams {
   section_id: string;
@@ -27,8 +27,6 @@ class NewSection extends React.Component<IProps, any> {
   public handleFile = (e: React.SyntheticEvent<HTMLInputElement>) => {
     if (e.currentTarget.files) {
       Papa.parse(e.currentTarget.files[0], {
-        header: true,
-        skipEmptyLines: true,
         complete: result => {
           const data = _.groupBy(result.data, "section");
           const sections = Object.keys(data);
@@ -36,7 +34,9 @@ class NewSection extends React.Component<IProps, any> {
             .map(section => data[section].length)
             .reduce((acc, cur) => acc + cur);
           this.setState({ data, sections, students });
-        }
+        },
+        header: true,
+        skipEmptyLines: true
       });
     }
   };
