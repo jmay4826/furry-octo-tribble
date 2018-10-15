38ce9abf8e67665cf57ef8b5ea6194447dc4b572
Jonathan May <jmay4826@gmail.com>
2018-10-15 16:40:27 -0600

    Add blank section creation

--- src/components/NewSection.tsx
+++ src/components/NewSection.tsx
@@ -1,63 +1,45 @@
-import { faFileUpload } from "@fortawesome/free-solid-svg-icons";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Axios from "axios";
-import * as _ from "lodash";
-import * as Papa from "papaparse";
+import { Field, FieldArray, Form, Formik, ErrorMessage } from "formik";
 import * as React from "react";
 import { RouteComponentProps } from "react-router";
+import * as Yup from "yup";
+import { BulkSectionUpload } from "./BulkSectionUpload";
 
 interface IParams {
   section_id: string;
 }
 
 interface IProps extends RouteComponentProps<IParams> {
-  section?: ISection;
+  refreshSections: () => Promise<void>;
 }
 
 class NewSection extends React.Component<IProps, any> {
-  constructor(props: IProps) {
-    super(props);
-    this.state = {
-      data: undefined,
-      sections: [],
-      students: 0,
-      bulk: false
-    };
-  }
-
-  public handleFile = (e: React.SyntheticEvent<HTMLInputElement>) => {
-    if (e.currentTarget.files) {
-      Papa.parse(e.currentTarget.files[0], {
-        complete: result => {
-          const data = _.groupBy(result.data, "section");
-          const sections = Object.keys(data);
-          const students = sections
-            .map(section => data[section].length)
-            .reduce((acc, cur) => acc + cur);
-          this.setState({ data, sections, students });
-        },
-        header: true,
-        skipEmptyLines: true
-      });
-    }
-  };
-
-  public handleChange = ({
-    currentTarget: { name, value }
-  }: React.SyntheticEvent<HTMLInputElement>) =>
-    this.setState({ [name]: value });
-
-  public handleSubmit = () => {
-    Axios.post("/api/sections/", this.state.data, {
+  public validationSchema = Yup.object().shape({
+    collaborators: Yup.array().of(Yup.string()),
+    description: Yup.string().required(),
+    id: Yup.string().required()
+  });
+  public handleSubmit = (values: any) => {
+    console.log(values);
+    // Axios;
+    Axios.post("/api/sections/", values, {
       headers: { Authorization: `Bearer ${localStorage.getItem("token")}` }
     })
-      .then(res => console.log(res))
-      .catch(err => console.log(err));
+      .then(res => {
+        this.props
+          .refreshSections()
+          .then(() => this.props.history.push(`/sections/${res.data.id}`));
+      })
+      .catch(err => console.log("error", err));
   };
   public render() {
     return (
       <React.Fragment>
-        <div className="messages-header">Add a new section</div>
+        <div className="messages-header">
+          <span style={{ visibility: "hidden" }}>Add a section</span>
+        </div>
         <div
           className="messages-list"
           style={{
@@ -66,53 +48,140 @@ class NewSection extends React.Component<IProps, any> {
             flexDirection: "column"
           }}
         >
-          {!this.state.bulk ? (
-            <React.Fragment>
-              <p>Create an empty section</p>
-              <label htmlFor="name">
-                <input
-                  autoComplete="new-name"
-                  onChange={this.handleChange}
-                  value={this.state.name}
-                  type="text"
-                  name="name"
-                  placeholder="Name (must be unique)"
-                />
-              </label>
-              <label htmlFor="description">
-                <input
-                  autoComplete="new-description"
-                  onChange={this.handleChange}
-                  value={this.state.description}
-                  type="text"
-                  name="description"
-                  placeholder="Section Description"
-                />
-              </label>
-              <button>Clear</button>
-              <button onClick={this.handleSubmit}>Submit</button>
-            </React.Fragment>
-          ) : (
-            <React.Fragment>
-              <label htmlFor="upload">
-                <FontAwesomeIcon icon={faFileUpload} size="3x" />
-                <input
-                  type="file"
-                  name="upload"
-                  accept=".csv"
-                  onChange={this.handleFile}
-                />
-              </label>
-              <p>Upload section and student information</p>
-              <p>{this.state.sections.length} sections</p>
-              <p>{this.state.students} students total</p>
-              <p>
-                You can upload a CSV file with student information. Make sure
-                the columns are:
-              </p>
-              <p>firstname | lastname | email | password | section </p>
-            </React.Fragment>
-          )}
+          <div className="conversation-preview">
+            <Formik
+              initialValues={{
+                collaborators: [""],
+                description: "",
+                id: "",
+                type: "blank"
+              }}
+              onSubmit={this.handleSubmit}
+              validationSchema={this.validationSchema}
+            >
+              {({ values, submitForm, errors }: any) => {
+                console.log(errors);
+                return (
+                  <Form>
+                    <div
+                      style={{
+                        display: "flex",
+                        justifyContent: "space-around"
+                      }}
+                    >
+                      <h3
+                        style={{
+                          alignSelf: "center",
+                          flexGrow: "unset",
+                          marginLeft: "10px"
+                        }}
+                      >
+                        Add a...
+                      </h3>
+                      <Field
+                        type="radio"
+                        name="type"
+                        checked={values.type === "blank"}
+                        value="blank"
+                        id="blank"
+                      />
+                      <label
+                        className={`role ${
+                          values.type === "blank" ? "selected" : ""
+                        }`}
+                        htmlFor="blank"
+                      >
+                        Blank Section
+                      </label>
+                      {/* <Field
+                        type="radio"
+                        name="type"
+                        checked={values.type === "withStudents"}
+                        value="withStudents"
+                        id="withStudents"
+                      />
+                      <label
+                        className={`role ${
+                          values.type === "withStudents" ? "selected" : ""
+                        }`}
+                        htmlFor="withStudents"
+                      >
+                        Section with Students
+                      </label> */}
+                      <Field
+                        type="radio"
+                        name="type"
+                        checked={values.type === "collaborator"}
+                        value="collaborator"
+                        id="collaborator"
+                      />
+                      <label
+                        className={`role ${
+                          values.type === "collaborator" ? "selected" : ""
+                        }`}
+                        htmlFor="collaborator"
+                      >
+                        Section as a Collaborator
+                      </label>
+                    </div>
+                    {values.type === "blank" && (
+                      <React.Fragment>
+                        <Field
+                          autoComplete="new-id"
+                          type="text"
+                          name="id"
+                          placeholder="ID (must be unique)"
+                        />
+                        <ErrorMessage name="id" />
+
+                        <Field
+                          autoComplete="new-description"
+                          type="text"
+                          name="description"
+                          placeholder="Section Description"
+                        />
+                        <FieldArray name="collaborators">
+                          {({ push, remove }) => (
+                            <div>
+                              {!!values.collaborators.length &&
+                                values.collaborators.map(
+                                  (_: any, index: any) => (
+                                    <div
+                                      key={index}
+                                      style={{
+                                        alignItems: "center",
+                                        display: "flex"
+                                      }}
+                                    >
+                                      <Field
+                                        style={{ marginBottom: "10px" }}
+                                        name={`collaborators.${index}`}
+                                        placeholder="Collaborator Email (optional)"
+                                      />
+                                      <button onClick={() => remove(index)}>
+                                        <FontAwesomeIcon icon={faTimes} />
+                                      </button>
+                                    </div>
+                                  )
+                                )}
+                              <button type="button" onClick={() => push("")}>
+                                Add a collaborator
+                              </button>
+                              <button>Clear</button>
+                              <button type="submit">Submit</button>
+                            </div>
+                          )}
+                        </FieldArray>
+                      </React.Fragment>
+                    )}
+                    {values.type === "withStudents" && <BulkSectionUpload />}
+                    {values.type === "collaborator" &&
+                      "Coming soon. Please reach out to the collaborating instructor to be added."}
+                  </Form>
+                );
+              }}
+            </Formik>
+          </div>
         </div>
       </React.Fragment>
     );
