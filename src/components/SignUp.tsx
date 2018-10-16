3928f3dcf6b21c5ce35bf705cd4998a823e06b0f
Jonathan May <jmay4826@gmail.com>
2018-10-15 23:23:48 -0600

    Improve error handling and move user info to separate component

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -2,7 +2,7 @@ import { faCheck, faExclamation } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import Axios from "axios";
-import { ErrorMessage, Field, Form, Formik } from "formik";
+import { Field, Form, Formik } from "formik";
 import * as debounce from "p-debounce";
 import * as queryString from "query-string";
 import * as React from "react";
@@ -11,8 +11,14 @@ import { CSSTransition } from "react-transition-group";
 import { UserContext } from "src/App";
 import * as Yup from "yup";
 import { Input } from "./Input";
+import { UserInformation } from "./UserInformation";
 
-class SignUp extends React.Component<RouteComponentProps, any> {
+interface IState {
+  user?: IDecodedUser;
+  error: string;
+}
+
+class SignUp extends React.Component<RouteComponentProps, IState> {
   public query = queryString.parse(this.props.location.search);
   public initialValues = {
     confirm_password: "",
@@ -27,27 +33,39 @@ class SignUp extends React.Component<RouteComponentProps, any> {
   public validationSchema = Yup.object().shape({
     confirm_password: Yup.string()
       .oneOf([Yup.ref("password")], "Passwords do not match.")
-      .required(),
-    email: Yup.string().email(),
-    first_name: Yup.string().required(),
-    last_name: Yup.string().required(),
+      .required()
+      .label("Confirm Password"),
+    email: Yup.string()
+      .email()
+      .required()
+      .label("Email"),
+    first_name: Yup.string()
+      .required()
+      .label("First Name"),
+    last_name: Yup.string()
+      .required()
+      .label("Last Name"),
     password: Yup.string()
       .min(8)
-      .required(),
+      .required()
+      .label("Password"),
     role: Yup.string().oneOf(
       ["student", "instructor"],
       "Please choose a student or instructor account"
     ),
-    section_id: Yup.string().test(
-      "section_id",
-      "Could not find section.",
-      async value => (!value ? true : await this.debouncedSection(value))
-    )
+    section_id: Yup.string()
+      .test(
+        "section_id",
+        "Section ID is not valid. Try a different ID or remove it completely.",
+        async value => (!value ? true : await this.debouncedSection(value))
+      )
+      .label("Section ID")
   });
 
   constructor(props: RouteComponentProps) {
     super(props);
     this.state = {
+      error: "",
       user: undefined
     };
   }
@@ -66,31 +84,44 @@ class SignUp extends React.Component<RouteComponentProps, any> {
   public debouncedSection = debounce(this.validateInput("section_id"), 250, {});
 
   public onSubmit = async (values: any, functions: any) => {
-    const {
-      data: { token, user }
-    } = await Axios.post<{ token: string; user: IDecodedUser }>(
-      "/auth/signup",
-      values
-    );
-    localStorage.setItem("token", token);
-    this.setState({ user });
+    try {
+      const {
+        data: { token, user }
+      } = await Axios.post<{ token: string; user: IDecodedUser }>(
+        "/auth/signup",
+        values
+      );
+      localStorage.setItem("token", token);
+      this.setState({ user });
+    } catch (e) {
+      if (e.response.status === 409) {
+        this.setState({ error: e.response.data });
+      } else {
+        console.dir(e);
+      }
+    }
   };
 
   public render() {
     return this.state.user ? (
       <UserContext.Consumer>
         {({ setUser }) => {
-          setUser(this.state.user);
-          return (
-            <Redirect
-              push={true}
-              to={
-                this.state.user.role === "student"
-                  ? "/conversations"
-                  : "/sections"
-              }
-            />
-          );
+          if (this.state.user) {
+            setUser(this.state.user);
+
+            return (
+              <Redirect
+                push={true}
+                to={
+                  this.state.user.role === "student"
+                    ? "/conversations"
+                    : "/sections"
+                }
+              />
+            );
+          } else {
+            return null;
+          }
         }}
       </UserContext.Consumer>
     ) : (
@@ -107,47 +138,10 @@ class SignUp extends React.Component<RouteComponentProps, any> {
           onSubmit={this.onSubmit}
           validationSchema={this.validationSchema}
         >
-          {({ values, errors, resetForm, handleChange }: any) => {
+          {({ values, errors, resetForm, handleChange, touched }: any) => {
             return (
               <Form>
-                {JSON.stringify(errors)}
-                <div style={{ display: "flex" }}>
-                  <Field
-                    component={Input}
-                    label="First Name"
-                    name="first_name"
-                    type="text"
-                  />
-                  <Field
-                    component={Input}
-                    label="Last Name"
-                    name="last_name"
-                    type="text"
-                  />
-                  <ErrorMessage name="last_name" />
-                </div>
-                <div style={{ display: "flex" }}>
-                  <Field
-                    component={Input}
-                    label="Email Address"
-                    name="email"
-                    type="email"
-                  />
-                </div>
-                <div style={{ display: "flex" }}>
-                  <Field
-                    component={Input}
-                    label="Password"
-                    name="password"
-                    type="password"
-                  />
-                  <Field
-                    component={Input}
-                    label="Confirm Password"
-                    name="confirm_password"
-                    type="password"
-                  />
-                </div>
+                <UserInformation errors={errors} touched={touched} />
                 <div
                   style={{ display: "flex", justifyContent: "space-around" }}
                 >
@@ -205,6 +199,11 @@ class SignUp extends React.Component<RouteComponentProps, any> {
                         type="text"
                         name="section_id"
                         label="Section ID (optional)"
+                        inputStyle={
+                          errors.section_id && touched.section_id
+                            ? { border: "1px solid red" }
+                            : {}
+                        }
                       />
                       <FontAwesomeIcon
                         icon={errors.section_id ? faExclamation : faCheck}
@@ -239,6 +238,29 @@ class SignUp extends React.Component<RouteComponentProps, any> {
                   <button className="accent-button" type="submit">
                     Submit
                   </button>
+                </div>
+
+                <div
+                  className="error"
+                  style={{
+                    visibility:
+                      this.state.error ||
+                      (Object.keys(errors).length > 0 &&
+                        Object.keys(touched).length >= 6)
+                        ? "visible"
+                        : "hidden"
+                  }}
+                >
+                  Please correct these errors to submit:
+                  <ul>
+                    {Object.keys(errors).map(
+                      key =>
+                        touched[key] || key === "section_id" ? (
+                          <li key={key}>{errors[key]}</li>
+                        ) : null
+                    )}
+                    {this.state.error && <li>{this.state.error}</li>}
+                  </ul>
                 </div>
               </Form>
             );
